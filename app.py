dee98b6d90f211664d6cb1662f1ea8aa06f44ef0
Olle-88 <ollelundkvist1988@gmail.com>
2025-11-05 09:51:01 +0100

    Lagt till Flask-grund och GUI-startsida

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ping')
+def ping():
+    return jsonify({"status": "Servern körs!"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
